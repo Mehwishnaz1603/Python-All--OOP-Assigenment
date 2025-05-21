921c0b8ef07739f7ca6e00588dbce2c4a6acc743
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2025-05-21 20:15:58 +0500

    .

---
+++ Assigenment_02_using_cls_keyword/Assigenment_03_public_variables_and_methods/public_variables_and_methods.py
@@ -0,0 +1,16 @@
+#--------Assigenment 03---------#
+#Public Vriables And Methods:
+#Create a Class  Car witha public variable brand and a public method start(). 
+# Instentiate the class and access both from outside the class.
+
+class Car:
+    def __int_(self, brand):
+        self.brand = brand
+        def start(self):
+            print( f"The {self.brand} car is started.")
+if __name__ == "__main":
+    car1=Car( "Toyota")
+    print(car1.brand)
+    car1.start()
+        
+
